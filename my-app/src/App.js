c5c25e1580aba94267d128b7840a9513576b1301
molinarijess <lacerdajess@gmail.com>
2022-03-21 16:44:14 +0000

    added footer w/ github

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import Search from "./Search";
+import Github from "./Github";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather Search</h1>
       <Search />
+      <Github />
     </div>
   );
 }
---
+++ my-app/src/Github.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Github.css";
+
+export default function Github() {
+  return (
+    <footer>
+      Coded by{" "}
+      <a
+        href="https://github.com/molinarijess"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Jessica Molinari
+      </a>
+    </footer>
+  );
+}
