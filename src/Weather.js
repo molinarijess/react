bf7d1c7a12d9e0cb9d9ce6ba133465301238211c
molinarijess <lacerdajess@gmail.com>
2022-03-29 18:46:57 +0100

    added working form

--- src/Weather.js
+++ src/Weather.js
@@ -3,8 +3,23 @@ import CurrentDate from "./CurrentDate";
 import axios from "axios";
 
 export default function Weather({ defaultCity }) {
-  // const [searched, setSearched] = useState("");
+  const [city, setCity] = useState(defaultCity);
   const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function searchCity() {
+    const apiKey = "1f17ba351ee112a37d7633ae135f9016";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then((response) => {
+      setWeatherData({
+        ready: true,
+        temperature: Math.round(response.data.main.temp),
+        city: response.data.name,
+        description: response.data.weather[0].description,
+        date: new Date(response.data.dt * 1000),
+      });
+    });
+  }
+
   if (weatherData.ready) {
     return (
       <section id="frame">
@@ -16,33 +31,25 @@ export default function Weather({ defaultCity }) {
 
           <div className="weatherDescription">{weatherData.description}</div>
         </div>
-        <input
-          type="search"
-          placeholder="Change City"
-          className="search"
-          // onChange={(event) => {
-          //   event.target.value !== ""
-          //     ? setSearched(event.target.value)
-          //     : setSearched(defaultCity);
-          // }}
-          // onSubmit={(event) => {
-          //   event.preventDefault();
-          // }}
-        />
+        <form
+          onSubmitCapture={(event) => {
+            event.preventDefault();
+            searchCity();
+          }}
+        >
+          <input
+            type="search"
+            placeholder="Change City"
+            className="search"
+            onChange={(event) => {
+              setCity(event.target.value);
+            }}
+          />
+        </form>
       </section>
     );
   } else {
-    const apiKey = "1f17ba351ee112a37d7633ae135f9016";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(url).then((response) => {
-      setWeatherData({
-        ready: true,
-        temperature: Math.round(response.data.main.temp),
-        city: response.data.name,
-        description: response.data.weather[0].description,
-        date: new Date(response.data.dt * 1000),
-      });
-    });
+    searchCity();
     return null;
   }
 }
