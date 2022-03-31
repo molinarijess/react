ae7990f6c5b78e1ba9207372f31afc728d538ef0
molinarijess <lacerdajess@gmail.com>
2022-03-31 18:04:01 +0100

    organize files and displayed forecast vertically

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import ForecastDaily from "./ForecastDaily";
+import "./Forecast.css";
+import { AiOutlineLeft } from "react-icons/ai";
+import Weather from "./Weather";
+
 import axios from "axios";
 
 export default function Forecast({ coordinates }) {
@@ -9,18 +13,30 @@ export default function Forecast({ coordinates }) {
   if (loaded) {
     return (
       <div className="Forecast">
-        {" "}
-        {forecast.map((dailyForecast, index) => {
-          if (index < 3) {
-            return (
-              <div className="forecast-daily" key={index}>
-                <ForecastDaily data={dailyForecast} />
-              </div>
-            );
-          } else {
-            return null;
-          }
-        })}
+        <a
+          href="/"
+          className="back-to-weather"
+          onClick={() => {
+            <Weather />;
+          }}
+        >
+          <span className="back-to-weather">
+            <AiOutlineLeft />
+          </span>
+        </a>{" "}
+        <div className="forecast-show-data">
+          {forecast.map((dailyForecast, index) => {
+            if (index < 3) {
+              return (
+                <div className="forecast-daily" key={index}>
+                  <ForecastDaily data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
       </div>
     );
   } else {
--- src/ForecastDaily.js
+++ src/ForecastDaily.js
@@ -11,31 +11,15 @@ export default function ForecastDaily({ data }) {
   }
   return (
     <div className="ForecastDaily">
-      {/* <a
-        href="/"
-        className="back-to-weather"
-        onClick={() => {
-          <Weather />;
-        }}
-      >
-        <span className="back-to-weather">back</span>
-      </a> */}
-
-      <div className="forecast-grid">
-        <div className="forecast-week">{week()}</div>
-        <div>
-          <span className="forecast-max-temp">
-            {Math.round(data.temp.max)}ºC
-          </span>{" "}
-          |{" "}
-          <span className="forecast-min-temp">
-            {Math.round(data.temp.min)}ºC
-          </span>
-        </div>
-        <div className="forecast-description">
-          {data.weather[0].description}
-        </div>
+      <p className="forecast-week">{week()}</p>
+      <div>
+        <span className="forecast-max-temp">{Math.round(data.temp.max)}ºC</span>{" "}
+        |{" "}
+        <span className="forecast-min-temp">{Math.round(data.temp.min)}ºC</span>
       </div>
+      <small className="forecast-description">
+        {data.weather[0].description}
+      </small>
     </div>
   );
 }
